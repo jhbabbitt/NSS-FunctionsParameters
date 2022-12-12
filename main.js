0e1c73e85fc5093b3c280b34fa5757ccd9d1aebb
Jacob Babbitt <jacobhbabbitt@gmail.com>
2022-12-12 09:46:40 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const quarterValue = (bigNumber) => {
+    const quarter = bigNumber / 4
+    return quarter
+}
+
+// Invoke the function and store the return value
+const quarteredNumber = quarterValue(444)
+
+// Log the return value
+console.log(quarteredNumber)
